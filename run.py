b647b54f3a934eccda2291dbef1b09f41b3d4f2c
Julien Sicot <julien.sicot@gmail.com>
2018-10-21 19:24:52 +0200

    controlling if ppn exists

--- run.py
+++ run.py
@@ -13,18 +13,19 @@ for researcher in researchers:
     ppn = researcher['ppn']
     creator_names = researcher['firstname']+" "+researcher['lastname']
     collection_name = researcher['lastname'].lower()+"_"+researcher['ppn']
-    json_loaded = refs.getReferences(ppn)
-    biblio = refs.getRefsByRole(json_loaded, 'aut', creator_names)
-    total_items = len(biblio)
+    if ppn != "":
+        json_loaded = refs.getReferences(ppn)
+        biblio = refs.getRefsByRole(json_loaded, 'aut', creator_names)
+        total_items = len(biblio)
+        print(f"Pushing {total_items} items in Zotero bibliography : {collection_name}")
+        collection_id = pyzot.create_collection(collection_name)
+        # print(collection_id)
 
-    print(f"Pushing {total_items} items in Zotero bibliography : {collection_name}")
-    collection_id = pyzot.create_collection(collection_name)
-    # print(collection_id)
-
-    for i in range(0, total_items, 50):
-        start = i
-        if i+50 <= total_items:
-            end = i+50
-        else :
-            end = total_items
-        pyzot.create_items(collection_id, list(islice(biblio, start, end)))
+        for i in range(0, total_items, 50):
+            start = i
+            if i+50 <= total_items:
+                end = i+50
+            else :
+                end = total_items
+            pyzot.create_items(collection_id, list(islice(biblio, start, end)))
+            
