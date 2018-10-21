0838c10cd3236296cd685c84daa77bd8e48567f1
Julien Sicot <julien.sicot@gmail.com>
2018-10-21 20:27:58 +0200

    bug if only one role + docs returned by idref references service

--- retrieve_references.py
+++ retrieve_references.py
@@ -19,50 +19,61 @@ def getReferences(ppn):
     return json.loads(data)
     # print(j_obj)
 
+def getDatePubli(string):
+    date = ""
+    date_match = re.search("([0-9]{4})", string)
+    if hasattr(date_match, 'group'):
+        date = date_match.group()
+    return date
+
 def getRefsByRole(obj, aut_role, creator_names):
-    refs = obj['sudoc']['result']['role']
-    biblio = [] 
-    if len(refs) > 0 :
-        for role in refs :
-            if role['marc21Code'] == aut_role :
-                docs = role['doc']
-                date = ""
-                edition = ""
-                publisher = ""
-                if len(docs) > 0 :
-                    for d in docs:
-                        if d['referentiel'] == 'sudoc':
-                            citation = d['citation'].split("/")
-                            itemtype = 'book'
-                            title = citation[0]
-                            if len(citation) > 2:
-                                edition = citation[2]
-                                edition_list = citation[2].split(",")
-                                publisher = edition_list[0].split(":")[-1]
-                                if len(edition_list) > 1:
-                                    date_litt = edition_list[1]
-                                    if hasattr(date_litt, 'group'):
-                                        date = re.search("([0-9]{4})", date_litt).group()
-                            i = {'id':d['id'],'source':d['referentiel'],'itemtype':itemtype,'author': creator_names, 'title': title,'publisher': publisher, 'edition': edition, 'date': date, 'url': d['URL']}
-                        elif d['referentiel'] == 'bnf':
-                            citation = d['citation'].split("/")
-                            itemtype = 'book'
-                            title = citation[0]
-                            if len(citation) > 2:
-                                edition = citation[2]
-                                date = re.search("([0-9]{4})", edition).group()
-                            i = {'id':d['id'],'source':d['referentiel'],'itemtype':itemtype,'author': creator_names, 'title': title,'edition': edition, 'date': date, 'url': d['URL']}
-                        elif d['referentiel'] == 'theses':
-                            itemtype = 'thesis'
-                            i = {'id':d['id'],'itemtype':itemtype,'source':d['referentiel'],'author': creator_names,'title': d['citation'],'url': d['URL']}
-                        else :
-                            itemtype = 'journalArticle'
-                            i = {'id':d['id'],'itemtype':itemtype,'source':d['referentiel'],'author': creator_names,'title': d['citation'],'url': d['URL']}
-                        #print(i)
-                        biblio.append(i)
-    items = []
-    for i in biblio:
-        items.append(pyzot.prepare_item(i))
-    
-    return items
+    if len(obj['sudoc']['result']) > 0 :
+        if type(obj['sudoc']['result']['role']) != list:
+            refs = []
+            refs.append(obj['sudoc']['result']['role'])
+        else:
+            refs = obj['sudoc']['result']['role']
+        biblio = [] 
+        if len(refs) > 0 :
+            for role in refs :
+                if role['marc21Code'] == aut_role :
+                    docs = role['doc']
+                    date = ""
+                    edition = ""
+                    publisher = ""
+                    if len(docs) > 0 :
+                        for d in docs:
+                            if d['referentiel'] == 'sudoc':
+                                citation = d['citation'].split("/")
+                                itemtype = 'book'
+                                title = citation[0]
+                                if len(citation) > 2:
+                                    edition = citation[2]
+                                    edition_list = citation[2].split(",")
+                                    publisher = edition_list[0].split(":")[-1]
+                                    if len(edition_list) > 1:
+                                        date_litt = edition_list[1]
+                                        date = getDatePubli(date_litt)
+                                i = {'id':d['id'],'source':d['referentiel'],'itemtype':itemtype,'author': creator_names, 'title': title,'publisher': publisher, 'edition': edition, 'date': date, 'url': d['URL']}
+                            elif d['referentiel'] == 'bnf':
+                                citation = d['citation'].split("/")
+                                itemtype = 'book'
+                                title = citation[0]
+                                if len(citation) > 2:
+                                    edition = citation[2]
+                                    date = getDatePubli(edition)
+                                i = {'id':d['id'],'source':d['referentiel'],'itemtype':itemtype,'author': creator_names, 'title': title,'edition': edition, 'date': date, 'url': d['URL']}
+                            elif d['referentiel'] == 'theses':
+                                itemtype = 'thesis'
+                                i = {'id':d['id'],'itemtype':itemtype,'source':d['referentiel'],'author': creator_names,'title': d['citation'],'url': d['URL']}
+                            else :
+                                itemtype = 'journalArticle'
+                                i = {'id':d['id'],'itemtype':itemtype,'source':d['referentiel'],'author': creator_names,'title': d['citation'],'url': d['URL']}
+                            #print(i)
+                            biblio.append(i)
+        items = []
+        for i in biblio:
+            items.append(pyzot.prepare_item(i))
+        
+        return items
 
