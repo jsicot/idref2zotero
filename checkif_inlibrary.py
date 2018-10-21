f09aa43e441af679843adcc667a2658da6949ae1
Julien Sicot <julien.sicot@gmail.com>
2018-10-21 18:50:01 +0200

    script to do

---
+++ checkif_inlibrary.py
@@ -0,0 +1,5 @@
+"""
+Check if added items from idRef exist in the holdings of a specific library (by given an ILN or RCR).
+Putting items that are not existing in a specific zotero collection. 
+We will use ABES web services like multiwhere
+"""
